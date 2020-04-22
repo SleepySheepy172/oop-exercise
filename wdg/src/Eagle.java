ce1c23b5b46baf99c8917680439170f85d4b66b4
kikilagou <kiki-1@hotmail.co.uk>
2020-04-22 21:26:03 +0100

    eagle

--- wdg/src/Eagle.java
+++ wdg/src/Eagle.java
@@ -1,14 +1,20 @@
 import java.util.List;
+import java.util.Map;
 
 public class Eagle extends Animal {
 
-	protected Eagle(String name, String gender, int age, int lifespan, List<String> canEat) {
-		super(name, gender, age, lifespan, canEat);
+	protected Eagle(String name, String gender, int age, int lifespan, List<String> canEat, Map<String, Integer> foodStore) {
+		super(name, gender, age, lifespan, canEat, foodStore);
 	}
 
 	@Override
 	void eat(int amount, String food) {
-
+		if(getCanEat().contains(food)){
+			int numFoodItem = getFoodStore().get(food);
+			getFoodStore().put(food, numFoodItem-amount);
+		} else {
+			System.out.println(getName() + " can't eat " + food + "! They're an " + getClass());
+		}
 	}
 
 	@Override
