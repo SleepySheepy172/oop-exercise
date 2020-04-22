8ebf11821d06e3fc8ff923d9b923c6ae21839cb8
kikilagou <kiki-1@hotmail.co.uk>
2020-04-22 21:21:51 +0100

    food stuff

--- wdg/src/Animal.java
+++ wdg/src/Animal.java
@@ -1,26 +1,37 @@
 import java.util.List;
+import java.util.Map;
 
 public abstract class Animal {
 
+	private String name;
 	private String gender;
 	private int age;
+	private int lifespan;
 	private List<String> canEat;
+	private Map<String, Integer> foodStore;
 
-	protected Animal(String gender, int age, List<String> canEat) {
+	protected Animal(String name, String gender, int age, int lifespan, List<String> canEat, Map<String, Integer> foodStore) {
+		this.name = name;
 		this.gender = gender;
 		this.age = age;
+		this.lifespan = lifespan;
 		this.canEat = canEat;
+		this.foodStore = foodStore;
 	}
 
 	abstract void eat(int amount, String food);
 	abstract void move(int steps);
 	abstract void makeSound();
+	abstract void reproduce();
 
-	protected void getAnimalInfo() {
-//		print info about animal
+	public String getName() {
+		return name;
 	}
 
-	/* GETTERS */
+	public int getLifespan() {
+		return lifespan;
+	}
+
 	public String getGender() {
 		return gender;
 	}
@@ -37,11 +48,12 @@ public abstract class Animal {
 		this.age++;
 	}
 
+	public Map<String, Integer> getFoodStore() {
+		return foodStore;
+	}
+
 	public void addFoodToDiet(String foodItem) {
 		this.canEat.add(foodItem);
 	}
-
-
-
 
 }
---
+++ wdg/src/Eagle.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class Eagle extends Animal {
+
+	protected Eagle(String name, String gender, int age, int lifespan, List<String> canEat) {
+		super(name, gender, age, lifespan, canEat);
+	}
+
+	@Override
+	void eat(int amount, String food) {
+
+	}
+
+	@Override
+	void move(int steps) {
+
+	}
+
+	@Override
+	void makeSound() {
+
+	}
+
+	@Override
+	void reproduce() {
+
+	}
+}
