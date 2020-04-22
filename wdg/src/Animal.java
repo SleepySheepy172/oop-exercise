56cc51354a93b165a8de3cc9ca9fd3c9877ed2db
kikilagou <kiki-1@hotmail.co.uk>
2020-04-22 21:12:34 +0100

    animal

--- wdg/src/Animal.java
+++ wdg/src/Animal.java
@@ -4,15 +4,44 @@ public abstract class Animal {
 
 	private String gender;
 	private int age;
-	private int weight;
 	private List<String> canEat;
 
-	protected Animal(String gender, int age, int weight, List<String> canEat) {
+	protected Animal(String gender, int age, List<String> canEat) {
 		this.gender = gender;
 		this.age = age;
-		this.weight = weight;
 		this.canEat = canEat;
 	}
 
-	protected abstract void eat(int amount, String food);
+	abstract void eat(int amount, String food);
+	abstract void move(int steps);
+	abstract void makeSound();
+
+	protected void getAnimalInfo() {
+//		print info about animal
+	}
+
+	/* GETTERS */
+	public String getGender() {
+		return gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public List<String> getCanEat() {
+		return canEat;
+	}
+
+	public void ageOneYear() {
+		this.age++;
+	}
+
+	public void addFoodToDiet(String foodItem) {
+		this.canEat.add(foodItem);
+	}
+
+
+
+
 }
