7c6ae620cb32d4f2fc712e686295ced906a59b83
Janani <janani.jayanthan@publicissapient.com>
2020-04-24 14:45:28 +0100

    main

--- wdg/src/Main.java
+++ wdg/src/Main.java
@@ -1,17 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
 		List<String> canEat = new ArrayList<>();
 		canEat.add("cod");
 		canEat.add("salmon");
-		canEat.add("sardine")
+		canEat.add("sardine");
 
-		Map<String, Integer>foodStore = new HashMap<>();
+		Map<String, Integer> foodStore = new HashMap<>();
 		foodStore.put("cod", 8);
 		foodStore.put("salmon", 15);
 
 
-		Giraffe gerald = new Giraffe("Giraffe", "m", 2, 5, canEat, foodStore, 42);
-		gerald.eat(1, "cod");
+		Penguin petra = new Penguin("Petra", "m", 2, 5, canEat, foodStore, 42);
+		petra.eat(1, "cod");
 	}
 }
