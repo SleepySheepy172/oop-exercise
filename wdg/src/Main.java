affb2cc7e3c2423f229cf2843927e666ec02f93e
Janani <janani.jayanthan@publicissapient.com>
2020-04-24 14:41:24 +0100

    correct mistakes in main class

--- wdg/src/Main.java
+++ wdg/src/Main.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
 		List<String> canEat = new ArrayList<>();
 		canEat.add("eucalyptus");
 		canEat.add("leaves");
-		canEat.add("corn")
+		canEat.add("corn");
 
-		Map<String, Integer>foodStore = new HashMap<>();
+		Map<String, Integer> foodStore = new HashMap<>();
 		foodStore.put("eucalyptus", 800);
 		foodStore.put("corn", 150);
 
