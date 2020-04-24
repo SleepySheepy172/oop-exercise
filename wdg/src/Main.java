633fe08c826d3f960239261c8ba0dc82986503c4
Janani <janani.jayanthan@publicissapient.com>
2020-04-24 14:39:51 +0100

    imports

--- wdg/src/Main.java
+++ wdg/src/Main.java
@@ -1,17 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
 		List<String> canEat = new ArrayList<>();
 		canEat.add("seal");
 		canEat.add("grass");
-		canEat.add("cake")
+		canEat.add("cake");
 
-		Map<String, Integer>foodStore = new HashMap<>();
+		Map<String, Integer> foodStore = new HashMap<>();
 		foodStore.put("seal", 4);
 		foodStore.put("cake", 150);
 
 
-		Wolf walter = new Giraffe("walter", "m", 2, 5, canEat, foodStore, 96);
+		Wolf walter = new Wolf("walter", "m", 2, 5, canEat, foodStore, 96);
 		walter.eat(3, "cake");
 	}
 }
